3bd6f759b5d3623791f36810c8583928f685b57d
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-10-11 13:16:16 +0200

    save changes

--- Luchador.js
+++ Luchador.js
@@ -1,8 +1,12 @@
 import Personaje from "./Personajes";
 
 class Luchador extends Personaje {
+  tipoArma;
+  destreza;
   constructor(nombre, familia, estado, tipoArma, destreza) {
     super(nombre, familia, estado);
+    this.tipoArma = tipoArma;
+    this.destreza = destreza;
   }
 
   comunicar() {
