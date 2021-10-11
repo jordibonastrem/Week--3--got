f2d5a7faa0dbc401bc595236f4dfb395450b4ad5
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-10-11 13:22:39 +0200

    add rey

---
+++ Rey.js
@@ -0,0 +1,13 @@
+import Personaje from "./Personajes";
+
+class Rey extends Personaje {
+  agnosReinado;
+  constructor(nombre, familia, estado, agnosReinado) {
+    super(nombre, familia, estado);
+    this.agnosReinado = agnosReinado;
+  }
+
+  comunicar() {
+    return super.comunicar() + " vais a morir todos";
+  }
+}
